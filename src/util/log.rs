dc17b82171d241a2558d0f59298e07d5832161c0
Eldar Bakerman <eldar@tictuk.com>
2024-08-06 10:39:14 +0300

    feat: organise errors, alias errors

--- src/util/log.rs
+++ src/util/log.rs
@@ -16,6 +16,8 @@ pub use std::{
     println as trace, println as debug, println as info, println as warn, println as error,
 };
 
+use crate::{AliasError, Command};
+
 trait CustomLog: CologStyle {
     fn suffix_message(&self, level: &Level) -> String;
 
@@ -103,6 +105,12 @@ pub enum ErrorCode<'a> {
     RegexParse(&'a str, fancy_regex::Error) = 2,
     RegexValidationMatch(&'a Regex, &'a str, fancy_regex::Error),
     NoCommandInput,
+    NoAliasesInput,
+    ExpandAlias(&'a Command, AliasError),
+    InvalidName(String),
+    InvalidCommand(String),
+    InvalidAlias(String),
+    NoOutput,
 }
 
 impl<'a> ErrorCode<'a> {
@@ -114,14 +122,13 @@ impl<'a> ErrorCode<'a> {
         format!("Error {}: An unexpected error occured", self.discriminant())
     }
 
-    pub fn log(&self, function_name: &str) -> ! {
-        let message = match self {
+    pub fn log_debug(&self, function_name: &str) -> () {
+        match self {
             ErrorCode::RegexParse(regex, error) => {
                 debug!(
                     "[{function_name}] Could not parse regex {:?}. Error details: {error}",
                     regex
                 );
-                self.default_err()
             }
             ErrorCode::RegexValidationMatch(regex, input, error) => {
                 debug!(
@@ -129,13 +136,80 @@ impl<'a> ErrorCode<'a> {
                     regex,
                     input
                 );
-                self.default_err()
             }
-            ErrorCode::NoCommandInput => "No command provided. Please specify a command to execute. For help, use the '-h' or '--help' flag.".to_string(),
-            #[allow(unreachable_patterns)]
-            _ => self.default_err(),
+            ErrorCode::InvalidAlias(alias) => {
+                debug!("[{function_name}] Received invalid alias {:?}", alias);
+            }
+            ErrorCode::ExpandAlias(command, error) => {
+                debug!(
+                    "[{function_name}] Could not expand command {:?}. Received the following error: {:?}",
+                    command, error
+                );
+            }
+            ErrorCode::InvalidCommand(command) => {
+                debug!(
+                    "[{function_name}] Received an invalid command {:?}",
+                    command
+                );
+            }
+            ErrorCode::InvalidName(name) => {
+                debug!("[{function_name}] Received an invalid name {:?}", name);
+            }
+            ErrorCode::NoAliasesInput => {
+                debug!("[{function_name}] No aliases were passed to the program")
+            }
+            ErrorCode::NoCommandInput => {
+                debug!("[{function_name}] No command was passed to the program");
+            }
+            ErrorCode::NoOutput => {
+                debug!("[{function_name}] Couldn't find any matching aliases");
+            }
         };
+    }
 
-        panic!("{message}");
+    pub fn log_err(&self) -> () {
+        match self {
+            ErrorCode::NoCommandInput => {
+                error!("No command provided. Please specify a command to execute. For help, use the '-h' or '--help' flag.");
+            }
+            ErrorCode::NoAliasesInput => {
+                error!("No aliases provided. Please pipe the list of aliases into the command. For help, use the '-h' or '--help' flag.")
+            }
+            ErrorCode::InvalidAlias(alias) => {
+                // Technically, this error should never result in a panic,
+                // since we filter out invalid aliases.
+                error!(
+                    "The program encountered an invalid alias: {:?}.
+                Please remove or modify it to continue running.",
+                    alias
+                );
+            }
+            ErrorCode::NoOutput => {
+                error!("There is no alias matching your command.");
+            }
+            _ => error!("{}", self.default_err()),
+        }
+    }
+
+    pub fn log_and_panic(&self, function_name: &str) -> ! {
+        self.log_debug(function_name);
+        self.log_err();
+        std::process::exit(self.discriminant() as i32);
+    }
+}
+
+impl<'a> Into<i32> for ErrorCode<'a> {
+    fn into(self) -> i32 {
+        self.discriminant() as i32
+    }
+}
+
+impl<'a> From<AliasError> for ErrorCode<'a> {
+    fn from(value: AliasError) -> Self {
+        match value {
+            AliasError::InvalidCommand(command) => ErrorCode::InvalidCommand(command),
+            AliasError::InvalidName(name) => ErrorCode::InvalidName(name),
+            AliasError::ParseError(alias) => ErrorCode::InvalidAlias(alias),
+        }
     }
 }
